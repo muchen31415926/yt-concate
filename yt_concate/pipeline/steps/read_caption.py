6dd50822a699b9ae9b70adb26496f21f75acc9aa
Mu <muchen31415926@gmail.com>
2026-02-04 22:12:44 +0800

    Successfully read captions into a data structure

---
+++ yt_concate/pipeline/steps/read_caption.py
@@ -0,0 +1,29 @@
+import os
+from pprint import pprint
+
+from .step import Step
+from yt_concate.settings import CAPTIONS_DIR
+
+
+class ReadCaption(Step):
+    def process(self, data, inputs, utils):
+        data = {}
+        for caption_file in os.listdir(CAPTIONS_DIR):
+            captions = {}
+            with open(os.path.join(CAPTIONS_DIR, caption_file), 'r') as f:
+                time_line = False
+                time = None
+                caption = None
+                for line in f:
+                    if '-->' in line:
+                        time = line.strip()
+                        time_line = True
+                        continue
+                    if time_line:
+                        caption = line.strip()
+                        captions[caption] = time
+                        time_line = False
+            data[caption_file] = captions
+
+        pprint(data)
+        return data
